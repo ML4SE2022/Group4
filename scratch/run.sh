5a175d49f7c61f2ad185694db3d86491b353c5ad
Khalid <k.elhaji@student.tudelft.nl>
2022-10-29 15:06:12 +0200

    From scratch

---
+++ scratch/run.sh
@@ -0,0 +1,15 @@
+cd code
+python3 run.py \
+	--do_train \
+	--model_type roberta \
+	--model_name_or_path microsoft/codebert-base \
+	--config_name roberta-base \
+	--tokenizer_name roberta-base \
+	--train_filename ../data/train.java-cs.txt.java,../data/train.java-cs.txt.cs \
+	--output_dir /output \
+	--max_source_length 512 \
+	--max_target_length 512 \
+	--beam_size 5 \
+	--train_batch_size 4 \
+	--learning_rate 5e-5 \
+	--train_steps 50000
