f11a132f626751c67fae2e91e1c0dbdd137e3064
Robert Arntzenius from vm <robertarntzenius@github.com>
2022-10-29 18:54:49 +0000

    gitignore aangepast en updated run.sh

--- scratch/run.sh
+++ scratch/run.sh
@@ -1,15 +1,18 @@
 cd code
 python3 run.py \
 	--do_train \
+	--do_eval \
 	--model_type roberta \
 	--model_name_or_path microsoft/codebert-base \
 	--config_name roberta-base \
 	--tokenizer_name roberta-base \
-	--train_filename ../data/train.java-cs.txt.java,../data/train.java-cs.txt.cs \
+	--train_filename ../short-data/train.java-cs.txt.java,../short-data/train.java-cs.txt.cs \
+        --dev_filename ../short-data/valid.java-cs.txt.java,../short-data/valid.java-cs.txt.cs \
 	--output_dir output \
 	--max_source_length 512 \
 	--max_target_length 512 \
 	--beam_size 5 \
-	--train_batch_size 4 \
+	--train_batch_size 8 \
 	--learning_rate 5e-5 \
-	--train_steps 50000
+	--train_steps 1000 \
+	--eval_steps 500
