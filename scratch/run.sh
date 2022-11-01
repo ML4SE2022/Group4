2947498d0d1a600713935f5ea930d12bd6f2f7b5
Robert Arntzenius from vm <robertarntzenius@github.com>
2022-11-01 11:01:29 +0000

    working version from instance

--- scratch/run.sh
+++ scratch/run.sh
@@ -6,13 +6,13 @@ python3 run.py \
 	--model_name_or_path microsoft/codebert-base \
 	--config_name roberta-base \
 	--tokenizer_name roberta-base \
-	--train_filename ../short-data/train.java-cs.txt.java,../short-data/train.java-cs.txt.cs \
-        --dev_filename ../short-data/valid.java-cs.txt.java,../short-data/valid.java-cs.txt.cs \
+	--train_filename ../data/train.java-cs.txt.java,../data/train.java-cs.txt.cs \
+        --dev_filename ../data/valid.java-cs.txt.java,../data/valid.java-cs.txt.cs \
 	--output_dir output \
 	--max_source_length 512 \
 	--max_target_length 512 \
 	--beam_size 5 \
 	--train_batch_size 8 \
 	--learning_rate 5e-5 \
-	--train_steps 1000 \
-	--eval_steps 500
+	--train_steps 50000 \
+	--eval_steps 5000
